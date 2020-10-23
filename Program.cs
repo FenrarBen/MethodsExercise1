8423113f1c85e87334c963617ef1fe06afe925c8
BenStrawbridge <lute_midnight@yahoo.com>
2020-10-23 12:28:19 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your quest?");
+            string quest = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine(name + " approaches the bridge keeper.  He tells the bridge keeper "
+                + "\"My name is " + name + ", My quest is: " + quest + ", and my favorite color is "
+                + color + ". Now let me pass or I shall bite my thumb at you.\"");
+        }
+    }
+}
